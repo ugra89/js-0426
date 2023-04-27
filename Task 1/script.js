bcdb26abf9a299072d3e50eccfbcb0b347eed65b
ugra89 <ugra89@gmail.com>
2023-04-27 11:43:02 +0300

    update count

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,41 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const output = document.querySelector('#output');
+const outputpounds = document.createElement('div');
+const outputgrams = document.createElement('div');
+const outputounces = document.createElement('div');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  function convert(primaryValue) {
+    const pounds = primaryValue * 2.2046;
+    const grams = primaryValue / 0.001;
+    const ounces = primaryValue * 35.274;
+
+    const resultOutputpounds = document.createElement('p');
+    resultOutputpounds.textContent = `Jūsų svoris (${primaryValue}kg) yra lygus ${pounds.toFixed(
+      2
+    )} svarais.`;
+    const resultOutputgrams = document.createElement('p');
+    resultOutputgrams.textContent = `Jūsų svoris (${primaryValue}kg) yra lygus ${grams.toFixed(
+      2
+    )} gramais.`;
+    const resultOutputounces = document.createElement('p');
+    resultOutputounces.textContent = `Jūsų svoris (${primaryValue}kg) yra lygus ${ounces.toFixed(
+      2
+    )} uncijomis.`;
+    outputpounds.append(resultOutputpounds);
+    outputgrams.append(resultOutputgrams);
+    outputounces.append(resultOutputounces);
+    output.appendChild(outputpounds);
+    output.appendChild(outputgrams);
+    output.appendChild(outputounces);
+  }
+
+  const kg = Number(input.value);
+  convert(kg);
+});
