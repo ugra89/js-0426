ed9bb431339da69274debab3d2ba15364a23891a
ugra89 <ugra89@gmail.com>
2023-05-01 19:01:36 +0300

    final

--- Task 4/script.js
+++ Task 4/script.js
@@ -7,18 +7,30 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
-
 const ENDPOINT = 'cars.json';
-try {
-  fetch(ENDPOINT)
-    .then((response) => response.json())
-    .then((cars) => {
-      cars.sort((a, b) => a.brand.localeCompare(b.brand));
-      console.log(cars);
+
+const carsList = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const cars = await response.json();
+    cars.sort((a, b) => a.brand.localeCompare(b.brand));
+    console.log(cars);
+    const output = document.getElementById('output');
+
+    cars.forEach((car) => {
+      const card = document.createElement('div');
+      card.classList.add('cars-Card');
+      card.innerHTML = `<h2>Brand:
+       ${car.brand}</h2> <h3>Models:</h3> <ul>${car.models
+        .map((model) => `<li>${model}</li>`)
+        .join('')}</ul>`;
+      output.append(card);
     });
-} catch (err) {
-  console.error(err);
-}
+  } catch (err) {
+    console.error(err);
+  }
+};
+carsList();
 
 /**  <main>
       <div class="wrapper">
