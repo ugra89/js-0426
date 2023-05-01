7e1aacc7ea83a0c8338f9a0c720b97d9e09daba7
ugra89 <ugra89@gmail.com>
2023-05-01 18:12:17 +0300

    last changes

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,6 +1,13 @@
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
+import { composition } from './modules/math/composition';
+import { division } from './modules/math/division';
+import { substraction } from './modules/math/subtraction';
+import { multiplication } from './modules/math/multiplication';
+import { one, two, three, four, five } from './modules/numbers/numbers';
+
+console.log('composition', composition);
 
 let a = composition(one, four);
 let b = division(four, two);
--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,17 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calculator() {
+  this.sum = function (a, b) {
+    return a + b;
+  };
+  this.substraction = function (a, b) {
+    return a - b;
+  };
+  this.multiplication = function (a, b) {
+    return a * b;
+  };
+  this.division = function (a, b) {
+    return a / b;
+  };
+}
